498e678477d15c986b30bfc7884352965fd9a4a8
ihkim <ihkim4@humaxmobility.co.kr>
2026-03-11 00:33:11 +0900

    bug fix

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,19 @@
+const MASTERS = [999846, 981141];
+
 function isMaster(chat) {
-  const master = Number(chat?.clientChannelId);
-  return (
-    master === "999846" ||
-    master === "981141" 
-  );
+  const clientId = Number(chat?.clientChannelId);
+  return MASTERS.includes(clientId);
 }
 
 function isManager(chat, channelId) {
-  const master = Number(chat?.clientChannelId);
-  return (
-    master === "999846" ||
-    master === "981141" ||
-    master === String(channelId) ||
-    chat?.role === "M"
-  );
+  const clientId = Number(chat?.clientChannelId);
+  const chId = Number(channelId);
+
+  if (MASTERS.includes(clientId)) return true;
+  if (clientId === chId) return true;
+  if (chat?.role === "M") return true;
+
+  return false;
 }
 
 module.exports = {
